35a0b203fed476786139085c183fc300da6907c0
brian <brian.rivera.proboste@gmail.com>
2020-09-05 15:57:06 -0400

    calculator running, missing layout fixes, symbol input together and testing

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './styles.css'
 import { Keys } from './Keys';
 
+//if the initial value is not sent the initial value is equal to 0
 export const Calculator = ( {initialValue = 0} ) => {
 
-    
-    //developer.mozilla 
+    /*
+    hook useState to control both the result and the exercise
+    */
+    const [exercise, setExercise] = useState({
+        ex: `${initialValue}`,
+        res: initialValue
+    });
 
+
+    //function in charge of updating the result
+    /*
+    1.-handle errors using the try catch
+    2.-generates the result taking the value of the exercise saved in the state object,
+       using a new Function, it could also have been done with an eval () but according to: 
+       https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/eval
+       "executes the code which is passed with the privileges of the caller.
+       If you run eval () with a string of characters that could be affected
+       by a malicious element, you could end up executing malicious code
+       within the user's computer with the permissions of its page or web extension."
+    3.-update the result state 
+    */
+    const handleResult = () => {
+     try {          
+            const result =  new Function('return ' + exercise.ex)();
+            setExercise({...exercise,res:result});
+     } catch (error) {
+         console.log(error);
+     }
+    }
+       
     return (
         <div className="container login-container">
             <div className="col-md-6 calculorBase mx-auto">
                 <br/>
                 <div className="resultCalculator">
-                    <h5>{initialValue}</h5>
+                    <h5>{exercise.ex}</h5>
                     <hr/>
-                    <h3>{initialValue}</h3>
+                    <h3>{exercise.res}</h3>
                 </div>
                 <div className="numberCalculator">
-                    <Keys></Keys>
+                    <div className="row">
+                        <Keys setExercise={setExercise}></Keys>
+
+                        <div className="col-sm-3 resultDiv">  
+                            <div className="divBtnResult bg-primary text-white" onClick={handleResult}>
+                               <h1>=</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <br/>
             </div>
@@ -26,6 +63,7 @@ export const Calculator = ( {initialValue = 0} ) => {
     )
 }
 
+// propType, is used to designate the initialValue as a variable of type numeric
 Calculator.propTypes = {
     initialValue : PropTypes.number
 }
--- src/Components/Keys.jsx
+++ src/Components/Keys.jsx
@@ -1,76 +1,68 @@
 import React from 'react'
 
-export const Keys = () => {
+export const Keys = ({setExercise}) => {
 
+/*
+used to decrease the identical code of the buttons, the return is traversed
+the lists using the .map and creating the buttons
+*/
+    
+const keySimbol = [1,2,3,4,5,6,7,8,9,0,'.'];
+const keyNumeric = ['+','-','*','/'];
+
+/*
+function applied to all buttons with the exception of the AC and = to update the status of the exercise:
+1.-take button value
+
+2.-use the property of the setState (setExercise) that fetches the current value of the state 
+
+3.-valid, if the current exercise is '0' and the new value is '.' to add the
+new value to exercise, if not '.' replace 0 with the new number
+
+4.-copy the previous values ​​of the state and replace the value of the exercise, with the new value
+*/
+
+const handleSimbolNumber = (e) => {
+    const value = e.target.value;
+    setExercise(last => {
+        let lastEx = (last.ex === '0' && value !== '.') ? '' : last.ex;
+        return ({...last,ex:`${lastEx + value}`}) 
+    });
+};
+
+//returns the state values ​​to 0
+const handleReset = () => {
+    setExercise({ex: '0',res: 0});
+};
 
     return (
-                             <div className="row">
-                        <div className="col-sm-12">
-                            <div className="row">
-                            <div className="col-sm-3 contButton">
-                                <button type="button" className="btn btn-outline-dark">+</button>
+                <>
+                <div className="col-sm-12">
+                    <div className="row">
+                        {
+                           keyNumeric.map((simb,index) =>(
+                            <div key={index.toString()} className="col-sm-3 contButton">
+                                <button  type="button" className="btn btn-outline-dark" onClick={handleSimbolNumber} value={simb}>{simb}</button>
                             </div>
-                            <div className="col-sm-3 contButton">
-                                <button type="button" className="btn btn-outline-dark">-</button>
-                            </div>
-                            <div className="col-sm-3 contButton">
-                                <button type="button" className="btn btn-outline-dark">*</button>
-                            </div>
-                            <div className="col-sm-3 contButton">
-                                <button type="button" className="btn btn-outline-dark">/</button>
-                            </div>
-                        </div>
-
-                        </div>
-                        <div className="col-sm-9">
-                            <div className="row">
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">1</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">2</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">3</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">4</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">5</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">6</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">7</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">8</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">9</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">0</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-outline-dark">.</button>
-                            </div>
-                            <div className="col-sm-4 contButton">
-                                <button type="button" className="btn btn-danger">AC</button>
-                            </div>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-3 resultDiv">
-                            
-                            
-                        <div className="divBtnResult bg-primary text-white">
-                            <h1>=</h1>
-                        </div>
-                            
-                        </div>
+                           )) 
+                        }
                     </div>
+                </div>
+                <div className="col-sm-9">
+                    <div className="row">
+                        {
+                           keySimbol.map((numb,index) =>(
+                            <div key={index.toString()} className="col-sm-4 contButton">
+                                <button type="button" className="btn btn-outline-dark" onClick={handleSimbolNumber}  value={numb}>{numb}</button>
+                            </div>
+                           )) 
+                        }
+                        <div className="col-sm-4 contButton">
+                            <button type="button" className="btn btn-danger" onClick={handleReset}>AC</button>
+                        </div>
+
+                    </div>
+                </div>
+                </>
     )
-}
+};
